10699ada2c8658f8f2fde64aaec59a22cbea593d
PhilippCo <philipp.cochems@gmail.com>
2021-09-10 19:48:31 +0200

    create rebuild_gpib

---
+++ rebuild_gpib.sh
@@ -0,0 +1,13 @@
+#install Kernel Module
+cd /usr/local/src/linux-gpib-code/linux-gpib-kernel/
+sudo make clean
+sudo make
+sudo make install
+
+#install User Module
+cd /usr/local/src/linux-gpib-code/linux-gpib-user/
+sudo make clean
+sudo ./bootstrap
+sudo ./configure
+sudo make
+sudo make install
